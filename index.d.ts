73953a2a18c238673f8b9c3956c7b96ffc25d1a2
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-12-31 16:23:31 -0800

    update: fix export

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ import postcss from "postcss";
 import { Plugin } from "esbuild";
 
 declare interface Options {
-    plugins: postcss.Plugin;
+    plugins: postcss.Plugin & postcss.ExportedAPI;
 }
 
 declare const esbuildPluginPostCss: (options: Options) => Plugin;
